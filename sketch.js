3f4e55056ce24922aa23292ed20ad595e444dabe
xxLizGxx <lgoltsova2014@gmail.com>
2021-01-30 09:51:30 +0000

    added platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150, 300, 300, 180)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +37,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+
 }
 
 function draw(){
@@ -43,6 +46,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    platform.display();
     box1.display();
     box2.display();
     ground.display();
